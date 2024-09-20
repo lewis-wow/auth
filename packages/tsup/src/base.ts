c9f30da428c01ac3b8cf74e3169e03ca841d182d
Ludvík Prokopec <prokolud@fit.cvut.cz>
2024-09-20 19:02:44 +0200

    generate esm and cjs

--- packages/tsup/src/base.ts
+++ packages/tsup/src/base.ts
@@ -3,5 +3,5 @@ import type { Options } from 'tsup';
 export const baseTsupConfig: Options = {
   entry: ['src/**/*.{ts,tsx}', '!src/**/*.test.{ts,tsx}'],
   sourcemap: true,
-  format: 'esm',
+  format: ['esm', 'cjs'],
 };
